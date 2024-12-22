40f81bf3dbbfbe13cff0b060f33edd3f018df77a
qusay-alkalehh <qusay.alkalih@gmail.com>
2024-12-22 19:01:57 +0300

    integreted screens with stack of admin area

--- app/(adminRoot)/_layout.tsx
+++ app/(adminRoot)/_layout.tsx
@@ -1,10 +1,15 @@
+import React from "react";
 import { Stack } from "expo-router";
-import "react-native-reanimated";
 
-export default function AdminLayout() {
+const AdminRoot = () => {
   return (
-    <Stack initialRouteName="(home)">
-      <Stack.Screen name="(home)" options={{ headerShown: false }} />
+    <Stack initialRouteName="admin-home" screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="admin-home" options={{ headerShown: false }} />
+      <Stack.Screen name="patients" />
+      <Stack.Screen name="guides" />
+      <Stack.Screen name="patient-analysis/:patientId" />
     </Stack>
   );
-}
+};
+
+export default AdminRoot;
