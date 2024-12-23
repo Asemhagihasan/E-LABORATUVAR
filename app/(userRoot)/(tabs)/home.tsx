edf58617a14fdc69dcf6e0724a8c02cbe6fd380b
qusay-alkaleh <qusay.alkalih@gmail.com>
2024-12-23 21:23:15 +0300

    added new screen for "manage-doctors"

--- app/(userRoot)/(tabs)/home.tsx
+++ app/(userRoot)/(tabs)/home.tsx
@@ -1,17 +1,31 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { TouchableOpacity } from "react-native";
 import { useLogout } from "@/hooks/auth/useLogout";
+import { useUser } from "@/hooks/auth/useUser";
 
 const Home = () => {
   const { logout, isPending } = useLogout();
+  const { user, isLoadingUser } = useUser();
+
   return (
-    <SafeAreaView className="flex-1 items-center justify-center h-full bg-white">
+    <SafeAreaView className="flex-1 bg-white p-4 gap-4">
+      <View className="flex-1">
+        <View className="flex-1 pr-4">
+          <Text className="text-xl font-bold">Admin Dashboard</Text>
+          <Text>{JSON.stringify(user?.user_metadata, null, 2)}</Text>
+          <Text className="text-lg font-medium text-gray-500">
+            Manage patient statuses, monitor doctor activities, and oversee
+            system settings efficiently.
+          </Text>
+        </View>
+      </View>
+
       <TouchableOpacity
         onPress={() => logout()}
         disabled={isPending}
-        className="p-5 rounded-md flex flex-row justify-center items-center bg-[#1D61E7] shadow-md shadow-neutral-400/70"
+        className="p-5 rounded-md flex flex-row justify-center items-center bg-red-600 shadow-md shadow-neutral-400/70"
       >
         <Text className="text-white text-lg font-bold">Logout</Text>
       </TouchableOpacity>
