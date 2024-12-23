1a8ac5b11680a8b88c7bde27c8a878e4eaf29278
qusay-alkaleh <qusay.alkalih@gmail.com>
2024-12-23 21:00:39 +0300

    solved issue of admin area "_layout"

---
+++ app/(adminRoot)/(home)/_layout.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Stack } from "expo-router";
+
+const AdminRoot = () => {
+  return (
+    <Stack initialRouteName="admin-home">
+      <Stack.Screen name="admin-home" options={{ headerShown: false }} />
+      <Stack.Screen name="mange-doctors" options={{ headerShown: false }} />
+      <Stack.Screen name="patients" options={{ headerShown: false }} />
+      <Stack.Screen name="guides" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="patient-analysis/:patientId"
+        options={{ headerShown: false }}
+      />
+    </Stack>
+  );
+};
+
+export default AdminRoot;
