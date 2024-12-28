dbdcb47d1f06d4ae534d6e065538362879e821ad
qusay-alkaleh <qusay.alkalih@gmail.com>
2024-12-28 20:01:08 +0300

    added logic code to manage-doctors component

--- app/(adminRoot)/manage-doctors.tsx
+++ app/(adminRoot)/manage-doctors.tsx
@@ -1,18 +1,21 @@
 import AddNewDoctor from "@/components/admin-root/AddNewDoctor";
+import DoctorDetails from "@/components/admin-root/DoctorDetails";
 import GoBack from "@/components/ui/GoBack";
-import { SafeAreaView, Text, View } from "react-native";
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const ManageDoctors = () => {
   return (
     <SafeAreaView className="flex-1 bg-white p-4">
       <GoBack />
-      <View className="px-6 gap-4">
-        <Text className="text-2xl text-neutral-900 font-bold text-center mt-6">
+      <View className="px-6 mb-3">
+        <Text className="text-2xl text-neutral-900 font-bold text-center mb-4">
           Monitoring Doctor Activities
         </Text>
-
         <AddNewDoctor />
       </View>
+
+      <DoctorDetails />
     </SafeAreaView>
   );
 };
